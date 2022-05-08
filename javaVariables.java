41adb04941a6a707f1825393c45e8e355638a8da
Fernando Jesus Coyla Alvarez <fcoylaa@unsa.edu.pe>
2022-05-08 10:12:58 -0500

    javaVariables tema trabajado

---
+++ javaVariables.java
@@ -0,0 +1,19 @@
+public class javaVariables{
+	public static void main(String[] args) {
+		String name = "Fernando";
+		int myNum = 20;
+		myNum = 25;  // myNum es ahora 25
+
+		/*Final Variables*/
+		final int myNumFin = 15;
+		myNumFin = 20;  // se genera un error: no se puede asignar un valor a una variable final
+
+		/*Otros Tipos*/
+		int myNumType = 5;//Tipo entero
+		float myFloatNum = 5.99f;//Tipo Float o Double (Numeros Decimales)
+		char myLetter = 'D';//Tipo Caracter
+		boolean myBool = true;//Tipo Booleano (True/False)
+		String myText = "Hello";//Tipo String (Cadena de Texto)
+	}	
+
+}
