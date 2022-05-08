41f550343b97090cf7569674579e6c4a59f3ada1
Fernando Jesus Coyla Alvarez <fcoylaa@unsa.edu.pe>
2022-05-08 09:58:57 -0500

    javaSyntax trabajado

---
+++ javaSyntax.java
@@ -0,0 +1,9 @@
+public class javaSyntax{
+	public static void main(String[] args){
+		String p1="En java debemos guardar el nombre del archivo con el mismo de la clase";
+		String p2="El sistema de salida println() nos permite realizar la impresion en pantalla";
+		System.out.println(p1);
+		System.out.println(p2);
+		System.out.println("Gracias al Sistema de salida println() mostramos este MENSAJE");
+	}
+}
