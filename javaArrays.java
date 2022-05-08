f4a249ba0d4553ad7cdd551d2f24756b4f6518f6
Fernando Jesus Coyla Alvarez <fcoylaa@unsa.edu.pe>
2022-05-08 12:15:06 -0500

    javaArrays parte3 realizada

--- javaArrays.java
+++ javaArrays.java
@@ -19,7 +19,22 @@ public class javaArrays{
 		//Tambien podemos hacer uso del bucle ForEach
 		for (String i : cars) {
  		 System.out.println(i);
-		} //De esta manera se realiza el recorrido de manera mas practica	
+		} //De esta manera se realiza el recorrido de manera mas practica
+		
+		/*Arrays Multidimensionales*/
+		//Son Arrays de Arrays
+		//Para crear Arrays Bidimensionales se agrega cada matriz dentro del conjunto de llaves
+		int[][] myNumbers = { {1, 2, 3, 4}, {5, 6, 7} }; //Aqui tenemos un Array con dos Arrays como elementos
+		//Para acceder a los elementos, especificamos el primer numero como la posicion del array y el segundo como la posicion del numero dentro del Array elegido
+		int x = myNumbers[0][2];
+		System.out.println(x); // Imprimira 3
+		//Para realizar el recorrido de la matriz bidimensional hacemos uso del Bucle FOR Anidado
+		for (int i = 0; i < myNumbers.length; ++i) {
+		      for(int j = 0; j < myNumbers[i].length; ++j) {
+		        System.out.println(myNumbers[i][j]);
+		      }
+	        }
+		
 	} 
 	
 }
