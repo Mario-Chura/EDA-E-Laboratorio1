9cf6ceb075672b34c91a589afa4baeb050dbf2e4
Icielo23 <viquise@unsa.edu.pe>
2022-05-08 15:03:11 -0500

    Segundo ejercicio

---
+++ JavaDataTypes.java
@@ -0,0 +1,18 @@
+public class JavaDataTypes {
+    public static void main(String[] args) {
+  
+        int numero;
+        char letra;
+        String texto;
+        
+        numero = 5;
+        letra ='M';
+        texto ="Holi";
+        
+        System.out.println(numero);
+        System.out.println(letra);
+        System.out.println(texto);
+       
+      }
+    
+}
