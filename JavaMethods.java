c99b1f1be7c733f6490fceae273ccabb0de38b4b
Icielo23 <viquise@unsa.edu.pe>
2022-05-08 22:24:03 -0500

    termine el 6to ejercicio

---
+++ JavaMethods.java
@@ -0,0 +1,12 @@
+public class JavaMethods {
+    static void myMethod() {
+        System.out.println("Namjoon >.< ");
+        System.out.println("marry me !!!");
+      }
+      public static void main(String[] args) {
+        myMethod();
+        myMethod();
+        myMethod();
+      }
+    }
+    
