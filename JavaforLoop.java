111d2b36c437c5e1cb8149a6bcc0edd022ebb4e8
Icielo23 <viquise@unsa.edu.pe>
2022-05-08 22:03:37 -0500

    termine el ejercicio5

---
+++ JavaforLoop.java
@@ -0,0 +1,9 @@
+public class JavaforLoop {
+          public static void main(String[] args) {
+          for (int i = 1; i <= 30; i = i + 2) { //se quiere numeros impares
+            System.out.println(i);
+          }  
+        }
+      }
+      
+
