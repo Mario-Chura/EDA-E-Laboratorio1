d25104395c45248b0ad5fc84617c656a6ec87e41
Mario-Chura <mchurapum@unsa.edu.pe>
2022-05-08 19:59:03 -0500

    Haciendo casting de entero a doble

---
+++ JavaTypeCasting.java
@@ -0,0 +1,6 @@
+public class JavaTypeCasting {
+    int entero = 26;
+    double decimal = entero; //Pasamos de un dato entero a un decimal
+    System.out.println(entero); //Imprime 26
+    System.out.println(decimal);// Imprimer 26.0 por que es un doble
+}
