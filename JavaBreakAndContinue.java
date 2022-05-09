b2b25b69acfe3cf1a643dcffb4ef7d81d31c7632
Mario-Chura <mchurapum@unsa.edu.pe>
2022-05-08 21:23:41 -0500

    Ejemplo de continue

--- JavaBreakAndContinue.java
+++ JavaBreakAndContinue.java
@@ -8,5 +8,12 @@ public class JavaBreakAndContinue {
 			}
 			System.out.println(i); // Solo imprime hasta 4 porque en 5 se rompe el ciclo		
 		}
+		// Ejemplo de continue
+		for (int i = 0; i < 7; i++) {
+			if (i == 3) {
+				continue;
+			}
+			System.out.println(i); // Imprime del 0 al  6 sin rectriccion cuando llega a ser  3
+		}
 	}
 }
