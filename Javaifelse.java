85d48232e4dae3b1e62eb6994ff295e3a34a5478
Icielo23 <viquise@unsa.edu.pe>
2022-05-08 21:53:05 -0500

    se termino ejercicio numero 4

--- Javaifelse.java
+++ Javaifelse.java
@@ -1,11 +1,13 @@
 public class Javaifelse {
     public static void main(String[] args) {
         System.out.println();
-        if (22 > 3) {
-            System.out.println("22 es mayor que 3");
-          }  
-        }
-      
-      
+        int time = 13;
+    if (time < 8) {
+    System.out.println("Ama ya me voy a la universidad");
+    } else if (time < 15) {
+      System.out.println("Ama volvi se cancelaron las clases");
+    }  else {
+      System.out.println("Ama mi comida plis >.<");
     }
-
+  }
+}
