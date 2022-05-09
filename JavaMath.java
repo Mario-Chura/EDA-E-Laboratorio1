da209f7b389f86ed8b6dce07422cbfc239b8297c
Mario-Chura <mchurapum@unsa.edu.pe>
2022-05-08 20:16:36 -0500

    Metodos maximo y minimo

---
+++ JavaMath.java
@@ -0,0 +1,13 @@
+public class JavaMath {
+
+	public static void main(String[] args) {
+		// Metodo maximo me retorna el maximo 
+		int maximo = Math.max(25, 87);
+		System.out.println(maximo); // Imprime 87
+		// Metodo minimo me retorna el valor minimo
+		int minimo = Math.min(15, 85);
+		System.out.println(minimo); // Imprime 15
+
+	}
+
+}
