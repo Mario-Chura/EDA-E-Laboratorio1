a036c5dcadf5978f066ab3414d3c10d46a795352
Icielo23 <viquise@unsa.edu.pe>
2022-05-08 20:57:54 -0500

    Tercer ejercicio

---
+++ Javastring.java
@@ -0,0 +1,10 @@
+public class Javastring {
+    public static void main(String[] args) {
+        String nombre = "Cielo";
+        System.out.println("mi nombre es " + nombre);
+        System.out.println("el largo de mi nombre es " + nombre.length());
+        System.out.println("mi nombre en mayuscula es " + nombre.toUpperCase());
+        System.out.println("mi nombre en mayuscula es " + nombre.toLowerCase());
+        System.out.println("la letra e de mi nombre esta en el indice " + nombre.indexOf("e"));
+    }
+}
