c67b7a159587082229019ca73089370803cd51e2
Mario-Chura <mchurapum@unsa.edu.pe>
2022-05-08 20:49:00 -0500

    Metodo Math sqrt abs y random

--- JavaMath.java
+++ JavaMath.java
@@ -7,7 +7,14 @@ public class JavaMath {
 		// Metodo minimo me retorna el valor minimo
 		int minimo = Math.min(15, 85);
 		System.out.println(minimo); // Imprime 15
-
+		// Metodo raiz cuadrada
+		double raiz = Math.sqrt(64);
+		System.out.println(raiz); // Imprime 8
+		// Metodo valor absoluto
+		double abs = Math.abs(-158.23);
+		System.out.println(abs); // Imprime 158.23
+		// Metodo random
+		int aleatorio = (int) (Math.random()*200);
+		System.out.println(aleatorio); // Imprime numero aleatorio del 0 al 200
 	}
-
 }
