08f15f0dcfbde3e9839ffa2e4a847e1780003f9a
Icielo23 <viquise@unsa.edu.pe>
2022-05-08 21:35:30 -0500

    se borro readme

---
+++ Javaifelse.java
@@ -0,0 +1,11 @@
+public class Javaifelse {
+    public static void main(String[] args) {
+        System.out.println();
+        if (22 > 3) {
+            System.out.println("22 es mayor que 3");
+          }  
+        }
+      
+      
+    }
+
