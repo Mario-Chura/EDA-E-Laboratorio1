7e0da01efa1f9cb40ec395e3ab7eddf8e0e76efe
Mario-Chura <mchurapum@unsa.edu.pe>
2022-05-08 21:31:22 -0500

    Ejemplo de while con continue y break

--- JavaBreakAndContinue.java
+++ JavaBreakAndContinue.java
@@ -15,5 +15,25 @@ public class JavaBreakAndContinue {
 			}
 			System.out.println(i); // Imprime del 0 al  6 sin rectriccion cuando llega a ser  3
 		}
+		
+		// Ejemplo while y break
+        int x = 0;
+        while(x < 10) {
+            if(x == 6) {
+                break;
+            }
+            System.out.println("i: " + x);
+            x++;
+        }
+        
+        System.out.println("el ultimo valor de i es: " + x);
+        
+        // Ejemplo while y continue
+        for (int i = 0; i < 10; i++) {
+            if(i == 6) {
+                continue;
+            }
+            System.out.println("i: " + i);
+        }
 	}
 }
