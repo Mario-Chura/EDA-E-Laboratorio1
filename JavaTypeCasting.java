caef71046af8dc869f26a0775d28a2ae7f85ddc1
Mario-Chura <mchurapum@unsa.edu.pe>
2022-05-08 20:06:53 -0500

    Otra manera de hacer casting

--- JavaTypeCasting.java
+++ JavaTypeCasting.java
@@ -3,4 +3,10 @@ public class JavaTypeCasting {
     double decimal = entero; //Pasamos de un dato entero a un decimal
     System.out.println(entero); //Imprime 26
     System.out.println(decimal);// Imprimer 26.0 por que es un doble
+    //Segunda manera de hacer casting
+    double decimal2 = 23.45d;
+    int entero2 = (int) decimal2;
+    System.out.println(decimal2); // Imprime 23.45
+    System.out.println(entero2); // Imprime solo la parte entera 23
 }
+
