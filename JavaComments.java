b9d5b8714232cb0fe7e92812091f702716932714
Mario-Chura <mchurapum@unsa.edu.pe>
2022-05-08 19:33:30 -0500

    Creando ejemplo de comentario

---
+++ JavaComments.java
@@ -0,0 +1,11 @@
+public class JavaComments {
+
+	public static void main(String[] args) {
+		// Esto es un comentario
+		System.out.println("Aprendiendo Java"); // Esto es otro comentario
+		/* Esto es un comentario
+		   en varias lineas
+		*/
+	}
+
+}
